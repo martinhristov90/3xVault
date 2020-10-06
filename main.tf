28b815a31cd72c8740b5cfefd2954ec4f5b3a155
Martin Hristov <mhristov@hashicorp.com>
2020-10-06 12:40:05 +0300

    running terraform fmt

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "vault-cluster-us" {
-    source = "./modules/module_us"
+  source = "./modules/module_us"
 
-    vault_common_ca_cert = module.vault_common_ca.vault_common_ca_cert
-    vault_common_ca_private_key = module.vault_common_ca.common_ca_private_key
-    random_id = random_pet.env.id
+  vault_common_ca_cert        = module.vault_common_ca.vault_common_ca_cert
+  vault_common_ca_private_key = module.vault_common_ca.common_ca_private_key
+  random_id                   = random_pet.env.id
 
-    vault_license = file("./license_vault.txt")
+  vault_license = file("./license_vault.txt")
 }
 
