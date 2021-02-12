7285350a23d418f6aca7586ab13864e7d8383527
Martin Hristov <mhristov@hashicorp.com>
2021-02-12 14:09:50 +0200

    adding outputs for IPs of the EC2 instances

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "vault-cluster-us-public-ips" {
+  value       = [for node in module.vault-cluster-us.vault-instance-public-ips : format("Name: %s - IP: %s",node.tags.Name,node.public_ip)]
+  description = "Prints public IPs for the nodes in the US cluster"
+}
+
+output "vault-cluster-eu-public-ips" {
+  value       = [for node in module.vault-cluster-eu.vault-instance-public-ips : format("Name: %s - IP: %s",node.tags.Name,node.public_ip)]
+  description = "Prints public IPs for the nodes in the EU cluster"
+}
+
+output "vault-cluster-ap-public-ips" {
+  value       = [for node in module.vault-cluster-ap.vault-instance-public-ips : format("Name: %s - IP: %s",node.tags.Name,node.public_ip)]
+  description = "Prints public IPs for the nodes in the AP cluster"
+}
