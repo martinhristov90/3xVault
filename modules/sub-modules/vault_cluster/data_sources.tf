625f99bf7b26bb6c5e71837dd65524db7e7bc10b
Martin Hristov <mhristov@hashicorp.com>
2023-05-15 17:13:25 +0300

    removing iam_data_source_account_id.tf file and moving content to data_sources.tf

--- modules/sub-modules/vault_cluster/data_sources.tf
+++ modules/sub-modules/vault_cluster/data_sources.tf
@@ -25,3 +25,6 @@ data "aws_subnet" "subnets" {
 
   id = aws_subnet.public_subnet[each.key].id
 }
+
+# Getting the AWS account ID, it is used to create a policy and make sure that the AWS secrets engine has only permissions on IAM accounts that start with "vault-"
+data "aws_caller_identity" "current_aws_account" {}
