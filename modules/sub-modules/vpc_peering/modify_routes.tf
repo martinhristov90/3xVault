098959e7a6d536f1459db576db4adea09376f853
Martin Hristov <mhristov@hashicorp.com>
2023-05-19 11:44:51 +0300

    adding vpc peering between DR and PR regions + including route entries, fixing readme for it

--- modules/sub-modules/vpc_peering/modify_routes.tf
+++ modules/sub-modules/vpc_peering/modify_routes.tf
@@ -1,3 +1,4 @@
+# Routes from HQ region start here
 # Route from HQ to DR
 resource "aws_route" "route_hq_to_dr" {
   provider = aws.hq_provider
@@ -22,6 +23,7 @@ resource "aws_route" "route_hq_to_pr" {
   depends_on = [aws_vpc_peering_connection.peering_source_to_pr, aws_vpc_peering_connection_accepter.peer_pr]
 }
 
+# Routes for DR region start here
 # Route from DR to HQ
 resource "aws_route" "route_dr_to_hq" {
 
@@ -35,6 +37,20 @@ resource "aws_route" "route_dr_to_hq" {
   depends_on = [aws_vpc_peering_connection.peering_source_to_dr, aws_vpc_peering_connection_accepter.peer_dr]
 }
 
+# Route from DR to PR
+resource "aws_route" "route_dr_to_pr" {
+
+  provider = aws.dr_provider
+
+  route_table_id = data.aws_route_table.route_table_id_dr_region.route_table_id
+
+  destination_cidr_block    = var.clusters.ap.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering_dr_to_pr.id
+
+  depends_on = [aws_vpc_peering_connection.peering_dr_to_pr, aws_vpc_peering_connection_accepter.peer_dr_pr]
+}
+
+# Routes for PR start here
 # Route from PR to HQ 
 resource "aws_route" "route_pr_to_hq" {
 
@@ -46,4 +62,17 @@ resource "aws_route" "route_pr_to_hq" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering_source_to_pr.id
 
   depends_on = [aws_vpc_peering_connection.peering_source_to_pr, aws_vpc_peering_connection_accepter.peer_pr]
+}
+
+# Route from PR to DR
+resource "aws_route" "route_pr_to_dr" {
+
+  provider = aws.pr_provider
+
+  route_table_id = data.aws_route_table.route_table_id_pr_region.route_table_id
+
+  destination_cidr_block    = var.clusters.eu.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering_dr_to_pr.id
+
+  depends_on = [aws_vpc_peering_connection.peering_dr_to_pr, aws_vpc_peering_connection_accepter.peer_dr_pr]
 }
