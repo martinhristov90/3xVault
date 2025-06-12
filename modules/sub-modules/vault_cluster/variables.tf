92faa42377da5e6fa09b5e2b95c8a4b5e4ce18f5
Martin Hristov <mhristov@hashicorp.com>
2025-06-12 15:57:50 +0300

    testing signing with minor change

--- modules/sub-modules/vault_cluster/variables.tf
+++ modules/sub-modules/vault_cluster/variables.tf
@@ -43,7 +43,7 @@ variable "vault_version" {
 }
 
 variable "vault_ec2_type" {
-  description = "The size of the EC2 instance"
+  description = "The size of the EC2 instance in AWS"
   type        = string
 
   validation {
